043911fdbace36ec4bb8ca5cbd3c66b3adb2e01d
korikori10 <33927361+korikori10@users.noreply.github.com>
2017-12-20 15:43:14 +0200

    lets start

---
+++ treasure/App_Code/TreasureAjax.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.IO;
+
+
+/// <summary>
+/// Summary description for TreasureAjax
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class TreasureAjax : System.Web.Services.WebService {
+
+    public TreasureAjax () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    //----------------------------------------------------------------
+    // Set a path point
+    //----------------------------------------------------------------
+    public void setPathPoint(double lat, double lng, string groupName)
+    {
+        treasureWS.TreasureWS treasure = new treasureWS.TreasureWS();
+        treasureWS.Point point = new treasureWS.Point();
+        point.Lat = lat;
+        point.Lng = lng;
+        treasure.setPathPoint(point, groupName);
+    }
+
+
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    //--------------------------------------------------------------------------
+    // Get the distance of the point for a specific group from the target point
+    //--------------------------------------------------------------------------
+    public string getDistance(double lat, double lng, string groupName)
+    {
+        try
+        {
+            treasureWS.TreasureWS treasure = new treasureWS.TreasureWS();
+            treasureWS.Point point = new treasureWS.Point();
+            point.Lat = lat;
+            point.Lng = lng;
+            double dist = treasure.getDistance(point, groupName);
+            treasure.setPathPoint(point, groupName);
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            string jsonString = js.Serialize(dist);
+            return jsonString;
+        }
+        catch (Exception ex)
+        {
+            throw ex;
+        }
+    }
+    ////--------------------------------------------------------------------------
+    //// Get the distance of the point for a specific group from the target point
+    ////--------------------------------------------------------------------------
+    //public string getDistance(double lat, double lng, string groupName)
+    //{
+    //    treasureWS.TreasureWS treasure = new treasureWS.TreasureWS();
+    //    treasureWS.Point point = new treasureWS.Point();
+    //    point.Lat = lat;
+    //    point.Lng = lng;
+    //    double dist = treasure.getDistance(point, groupName);
+    //    treasure.setPathPoint(point, groupName);
+    //    JavaScriptSerializer js = new JavaScriptSerializer();
+    //    string jsonString = js.Serialize(dist);
+    //    return jsonString;
+    //}
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    //--------------------------------------------------------------------------
+    // Set a target group for a specific point
+    //--------------------------------------------------------------------------
+    public string setTarget(double lat, double lng, string groupName)
+    {
+        treasureWS.TreasureWS treasure = new treasureWS.TreasureWS();
+        treasureWS.Point point = new treasureWS.Point();
+        point.Lat = lat;
+        point.Lng = lng;
+        treasure.setTargetPoint(point, groupName);
+        JavaScriptSerializer js = new JavaScriptSerializer();
+        string jsonString = js.Serialize("ok");
+        return jsonString;
+    }
+
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    //------------------------------------------------------
+    // Set a Point Of Interst (POI) for a specific group
+    //------------------------------------------------------
+    public string setPOI(double lat, double lng, string groupName, string name, string description, string imageUrl)
+    {
+        treasureWS.TreasureWS treasure = new treasureWS.TreasureWS();
+        treasureWS.POI poi = new treasureWS.POI();
+        poi.P = new treasureWS.Point();
+        poi.P.Lat = lat;
+        poi.P.Lng = lng;
+        poi.Name = name;
+        poi.Description = description;
+        poi.ImageUrl = imageUrl;
+        JavaScriptSerializer js = new JavaScriptSerializer();
+        string jsonString = js.Serialize("ok");
+        try
+        {
+            treasure.setPOI(poi, groupName);
+            jsonString = js.Serialize("ok");
+        }
+        catch (Exception ex) {
+            jsonString = js.Serialize("error in treasure.setPOI --- " + ex.Message);
+        }
+
+        return jsonString;
+    }
+
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    //------------------------------------------------
+    // Get the target point for a specific group 
+    //------------------------------------------------
+    public string getTarget(string groupName)
+    {
+        treasureWS.TreasureWS treasure = new treasureWS.TreasureWS();
+        treasureWS.Point point = treasure.getTarget(groupName);
+        JavaScriptSerializer js = new JavaScriptSerializer();
+        string jsonString = js.Serialize(point);
+        return jsonString;
+    }
+
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    //------------------------------------------------
+    // Get the list of the points along the path 
+    //------------------------------------------------
+    public string getPathList(string groupName)
+    {
+        treasureWS.TreasureWS treasure = new treasureWS.TreasureWS();
+        treasureWS.Point[] pointArr = treasure.getPathList(groupName);
+        JavaScriptSerializer js = new JavaScriptSerializer();
+        string jsonString = js.Serialize(pointArr);
+        return jsonString;
+    }
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    //------------------------------------------------
+    // Get the list of the POI along the path 
+    //------------------------------------------------
+    public string getPOIList(string groupName)
+    {
+        treasureWS.TreasureWS treasure = new treasureWS.TreasureWS();
+        treasureWS.POI[] poiArr = treasure.getPOIList(groupName);
+        JavaScriptSerializer js = new JavaScriptSerializer();
+        string jsonString = js.Serialize(poiArr);
+        return jsonString;
+    }
+
+    [WebMethod]
+    [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+    //------------------------------------------------
+    //------------------------------------------------
+    public string GetChallenge(double lat, double lng, string groupName)
+    {
+        treasureWS.TreasureWS treasure = new treasureWS.TreasureWS();
+        treasureWS.Point point = new treasureWS.Point();
+        point.Lat = lat;
+        point.Lng = lng;
+        //treasure.setTargetPoint(point, groupName);
+        treasureWS.Challenge chal = treasure.GetChallenge(point, groupName);
+        JavaScriptSerializer js = new JavaScriptSerializer();
+        string jsonString = js.Serialize(chal);
+        return jsonString;
+    }
+
+}
+
---
+++ treasure/ajaxCalls.js
@@ -0,0 +1,148 @@
+﻿//---------------------------------------------------------------------------
+// get the distance from your current position to a point to the server
+//---------------------------------------------------------------------------
+function getDistance(request, successCB, failureCB) {
+
+    dataString = JSON.stringify(request);
+    $.ajax({ // ajax call starts
+        url: 'TreasureAjax.asmx/getDistance',   // JQuery call to the server side method
+        data: dataString,    // the parameters sent to the server
+        type: 'POST',        // can be post or get
+        dataType: 'json',    // Choosing a JSON datatype
+        contentType: 'application/json; charset = utf-8', // of the data received
+        timeout: 5000, // timeout in miliseconds
+        success: successCB, // 
+        error: failureCB 
+        })// end of ajax call
+}
+
+//-----------------------------------------------------------------------
+// Send the path point to the server
+//-----------------------------------------------------------------------
+function setPathPoint(request,successCB,failureCB) {
+        dataString = JSON.stringify(request);
+        $.ajax({ // ajax call starts
+            url: 'TreasureAjax.asmx/setPathPoint',   // server side method
+            data: dataString,    // name without the xml extension
+            type: 'POST',
+            dataType: 'json', // Choosing a JSON datatype
+            contentType: 'application/json; charset = utf-8',
+            timeout: 5000, // timeout in miliseconds
+            success: successCB, // Variable data contains the data we get from serverside
+            error: failureCB
+            }) // end of ajax call
+}
+
+//-----------------------------------------------------------------------
+// get the list of the path points
+//-----------------------------------------------------------------------
+function getPathList(request,successCB,failureCB) {
+  //  var groupName = document.getElementById("groupNameTB").value;
+    dataString = JSON.stringify(request);
+
+    $.ajax({ // ajax call starts
+        url: 'TreasureAjax.asmx/getPathList',   // server side method
+        data: dataString,    // the parameters sent to the server
+        type: 'POST',
+        dataType: 'json', // Choosing a JSON datatype
+        contentType: 'application/json; charset = utf-8',
+        timeout: 5000, // timeout in miliseconds
+        success: successCB,
+        error: failureCB
+    }) // end of ajax call
+}
+
+//-----------------------------------------------------------------------
+// get the target point
+//-----------------------------------------------------------------------
+function getTarget(request,successCB,failureCB) {
+
+    dataString = JSON.stringify(request);
+    $.ajax({ // ajax call starts
+        url: 'TreasureAjax.asmx/getTarget',   // server side method
+        data: dataString, // the data sent to the server  
+        type: 'POST',
+        dataType: 'json', // Choosing a JSON datatype
+        contentType: 'application/json; charset = utf-8',
+        timeout: 5000, // timeout in miliseconds
+        success: successCB,
+        error: failureCB
+    }) // end of ajax call
+}
+
+//-----------------------------------------------------------------------
+// Send the target point to the server
+//-----------------------------------------------------------------------
+function setTarget(request, successCB, failureCB) {
+
+    dataString = JSON.stringify(request);
+
+    $.ajax({ // ajax call starts
+        url: 'TreasureAjax.asmx/setTarget',   // server side method
+        data: dataString,    // name without the xml extension
+        type: 'POST',
+        dataType: 'json', // Choosing a JSON datatype
+        contentType: 'application/json; charset = utf-8',
+        timeout: 5000, // timeout in miliseconds
+        success: successCB,
+        error: failureCB
+    }) // end of ajax call
+}
+
+
+//-----------------------------------------------------------------------
+// Send the Point Of Interst (POI) to the server
+//-----------------------------------------------------------------------
+function setPOI(request,successCB,failureCB) {
+
+    dataString = JSON.stringify(request);
+
+    $.ajax({ // ajax call starts
+        url: 'TreasureAjax.asmx/setPOI',   // server side method
+        data: dataString,    // the parameters sent to the server
+        type: 'POST',
+        dataType: 'json', // Choosing a JSON datatype
+        contentType: 'application/json; charset = utf-8',
+        timeout: 5000, // timeout in miliseconds
+        success: successCB,
+        error: failureCB
+    }) // end of ajax call
+}
+
+//-----------------------------------------------------------------------
+// get the point of interst (POI)  list
+//-----------------------------------------------------------------------
+function getPOIList(request,successCB, failureCB) {
+
+    dataString = JSON.stringify(request);
+
+    $.ajax({ // ajax call starts
+        url: 'TreasureAjax.asmx/getPOIList',   // server side method
+        data: dataString,    // parameters passed to the server
+        type: 'POST',
+        dataType: 'json', // Choosing a JSON datatype
+        contentType: 'application/json; charset = utf-8',
+        timeout: 10000, // timeout in miliseconds
+        success: successCB, 
+        error: failureCB
+    }) // end of ajax call
+}
+
+function getChallenge(request, successCB, errorCB) {
+
+    var dataString = JSON.stringify(request);
+
+    $.ajax({
+        url: 'TreasureAjax.asmx/GetChallenge',   // server side web service method
+        data: dataString,                          // the parameters sent to the server
+        type: 'POST',                              // can be also GET
+        dataType: 'json',                          // expecting JSON datatype from the server     
+        contentType: 'application/json; charset = utf-8', // sent to the server
+        timeout: 5000, // timeout in miliseconds
+        success: successCB,
+        error: function (request, error, xhr) {
+            errorCB(request.responseText);
+        }
+    });
+}
+
